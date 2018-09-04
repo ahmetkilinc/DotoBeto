8848b9d6fd387597153e00c15c20e08cd74b3084
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2018-09-04 11:39:13 +0300

    Google ile login işlemi tamamlandı.

--- app/src/main/java/net/ahmetkilinc/dotobeto/Login.java
+++ app/src/main/java/net/ahmetkilinc/dotobeto/Login.java
@@ -1,13 +1,198 @@
 package net.ahmetkilinc.dotobeto;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class Login extends AppCompatActivity {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 0 ;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private com.google.android.gms.common.SignInButton signInButton;
+    private GoogleApiClient mGoogleApiClient;
+    private Button signOutButton;
+    private TextView nameTextView;
+    private TextView emailTextView;
+
+    public FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        signInButton = findViewById(R.id.sign_in_button);
+        signOutButton = findViewById(R.id.sign_out_button);
+        nameTextView = findViewById(R.id.name_text_view);
+        emailTextView = findViewById(R.id.email_text_view);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this , new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                    }
+                } /* Bağlantı fail olup olmadığını dinliyoruz */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                user = firebaseAuth.getCurrentUser();
+                signInButton.setVisibility(View.GONE);
+                signOutButton.setVisibility(View.VISIBLE);
+                if (user != null) {
+                    // kullanıcı giriş yaptı
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+
+                    if(user.getDisplayName() != null)
+                        nameTextView.setText("HI " + user.getDisplayName().toString());
+                        emailTextView.setText(user.getEmail().toString());
+
+                } else {
+                    // kullanıcı çıkış yaptı
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                    // ...
+            }
+        };
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                signIn();
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FirebaseAuth.getInstance().signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                        new ResultCallback() {
+                            @Override
+                            public void onResult(@NonNull Result result) {
+
+                                signInButton.setVisibility(View.VISIBLE);
+                                signOutButton.setVisibility(View.GONE);
+                                emailTextView.setText(" ".toString());
+                                nameTextView.setText(" ".toString());
+                            }
+                        });
+            }
+// ..
+        });
+
     }
+
+
+    private void signIn(){
+
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+
+                //Google girişi başarılı oldu, Firebase ile haberleşiyoruz
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+
+            } else {
+            // Google girişi fail oldu
+            // ...
+            }
+        }
+    }
+
+
+
+
+    @Override
+    public void onStart() {
+
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        Toast.makeText(Login.this, "Giriş yapıldı, yönlendir.",
+                                Toast.LENGTH_SHORT).show();
+                        // giriş başarısız olduğunda toast mesajı gösterme
+
+                        if (!task.isSuccessful()) {
+
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
