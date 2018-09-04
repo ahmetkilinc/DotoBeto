8472d0988e63ceb2b193f00795332ec69eba77ad
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2018-09-04 10:31:46 +0300

    Splash erkanı tamamlandı.

--- app/src/main/java/net/ahmetkilinc/dotobeto/SplashScreen.java
+++ app/src/main/java/net/ahmetkilinc/dotobeto/SplashScreen.java
@@ -1,16 +1,43 @@
 package net.ahmetkilinc.dotobeto;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
 
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this, Login.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
     }
 }
