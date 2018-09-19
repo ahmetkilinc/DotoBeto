14db445d37c1306f040eb0833873a43850cd0e90
Ahmet Kılınç <ahmetkilinc3899@gmail.com>
2018-09-19 14:28:50 +0300

    +profil sayfası eklendi.

--- app/src/main/java/net/ahmetkilinc/dotobeto/Profile.java
+++ app/src/main/java/net/ahmetkilinc/dotobeto/Profile.java
@@ -1,14 +1,225 @@
 package net.ahmetkilinc.dotobeto;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.mikepenz.materialdrawer.util.AbstractDrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerImageLoader;
+import com.mikepenz.materialdrawer.util.DrawerUIUtils;
+
+import java.util.HashMap;
 
 public class Profile extends AppCompatActivity {
+
+
+    private GoogleApiClient mGoogleApiClient;
+
+    private String displayName, email, photoUrl, displayNameSession, emailSession;
+
+    // Session Manager Class
+    SessionManager session;
+
+    //drawer
+    private AccountHeader headerResult = null;
+    Drawer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_profile);
+
+
+        // Session class instance
+        session = new SessionManager(getApplicationContext());
+        session.checkLogin();
+
+        //Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
+        if (!session.isLoggedIn()){
+
+            startActivity(new Intent(Profile.this, Login.class));
+        }
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        displayNameSession = user.get(SessionManager.KEY_DISPLAYNAME);
+        emailSession = user.get(SessionManager.KEY_EMAIL);
+
+
+        //initialize and create the image loader logic
+        DrawerImageLoader.init(new AbstractDrawerImageLoader() {
+            @Override
+            public void set(ImageView imageView, Uri uri, Drawable placeholder, String tag) {
+                Glide.with(imageView.getContext()).load(uri).placeholder(placeholder).into(imageView);
+            }
+
+            @Override
+            public void cancel(ImageView imageView) {
+
+                Glide.clear(imageView);
+            }
+
+            @Override
+            public Drawable placeholder(Context ctx, String tag) {
+
+                //define different placeholders for different imageView targets
+                //default tags are accessible via the DrawerImageLoader.Tags
+                //custom ones can be checked via string. see the CustomUrlBasePrimaryDrawerItem LINE 111
+                if (DrawerImageLoader.Tags.PROFILE.name().equals(tag)) {
+
+                    return DrawerUIUtils.getPlaceHolder(ctx);
+                }
+                else if (DrawerImageLoader.Tags.ACCOUNT_HEADER.name().equals(tag)) {
+
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(com.mikepenz.materialdrawer.R.color.primary).sizeDp(56);
+                }
+                else if ("customUrlItem".equals(tag)) {
+
+                    return new IconicsDrawable(ctx).iconText(" ").backgroundColorRes(R.color.md_red_500).sizeDp(56);
+                }
+
+                //we use the default one for
+                //DrawerImageLoader.Tags.PROFILE_DRAWER_ITEM.name()
+
+                return super.placeholder(ctx, tag);
+            }
+        });
+        //image loader logic.
+
+        //profil eklendiği zaman düzenle. ->
+
+        //final IProfile profile = new ProfileDrawerItem().withName(displayName).withEmail(displayEmail).withIcon(displayPhotoUrl).withIdentifier(100);
+        //final IProfile profile = new ProfileDrawerItem().withName(displayNameSession).withEmail(emailSession).withIdentifier(100).withTextColor(Color.BLACK).withTextColorRes(R.color.colorPrimaryDark);
+        final IProfile profile = new ProfileDrawerItem().withName(displayNameSession).withEmail(emailSession);
+
+
+        headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withTranslucentStatusBar(true)
+                .withHeaderBackground(R.color.md_blue_grey_200)
+                .addProfiles(
+                        //profil ekleme kısmı, giriş yapılan verileri al ve ekle.
+                        profile
+
+                        //new ProfileSettingDrawerItem().withName("Add Account").withDescription("Add new GitHub Account").withIdentifier(PROFILE_SETTING)
+                        //new ProfileSettingDrawerItem().withName("Manage Account").withIcon(GoogleMaterial.Icon.gmd_settings).withIdentifier(100001)
+                )
+                .withSavedInstance(savedInstanceState)
+                .build();
+
+
+        //adding navigation drawer
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+
+        new DrawerBuilder().withActivity(this).build();
+
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem itemText = new PrimaryDrawerItem().withName("").withSelectable(false);
+
+        PrimaryDrawerItem itemCikis = new PrimaryDrawerItem().withIdentifier(1).withName("Çıkış").withSelectable(false).withIcon(
+                R.drawable.cikis);
+
+        PrimaryDrawerItem itemProfil = new PrimaryDrawerItem().withIdentifier(2).withName("Profil").withSelectable(false).withIcon(
+                R.drawable.profil);
+/*
+        PrimaryDrawerItem itemAyarlar = new PrimaryDrawerItem().withIdentifier(3).withName(getString(R.string.dn_settings)).withSelectable(false).withIcon(
+                R.drawable.ayarlar);
+
+        PrimaryDrawerItem itemKapat = new PrimaryDrawerItem().withIdentifier(4).withName(getString(R.string.dn_close)).withSelectable(false).withIcon(
+                R.drawable.cikis);*/
+        //SecondaryDrawerItem item2 = new SecondaryDrawerItem().withIdentifier(2).withName(R.string.navigation_item_settings);
+
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        itemText,
+                        itemProfil,
+                        itemCikis,
+                        //itemAtananGorevler,
+                        //itemDevamEdenGorevler,
+                        new DividerDrawerItem()
+                        //itemAyarlar,
+                        //itemKapat
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
+                        if (drawerItem != null){
+
+                            if(drawerItem.getIdentifier() == 1){
+
+                                FirebaseAuth.getInstance().signOut();
+
+                                session.logoutUser();
+
+                                Intent i = new Intent(getApplicationContext(), Login.class);
+                                i.setFlags(i.FLAG_ACTIVITY_CLEAR_TOP | i.FLAG_ACTIVITY_CLEAR_TASK | i.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+                            }
+
+                            /*if(drawerItem.getIdentifier() == 1){
+
+                                startActivity(new Intent(Gorevler.this, Gorevler.class));
+                            }
+
+                            else if (drawerItem.getIdentifier() == 2){
+
+                                startActivity(new Intent(Gorevler.this, DevamEdenGorevler.class));
+                            }
+
+                            else if (drawerItem.getIdentifier() == 3){
+
+                                startActivity(new Intent(Gorevler.this, Activity_Settings.class));
+                            }
+
+                            else if (drawerItem.getIdentifier() == 4){
+
+                                session.logoutUser();
+
+                                Intent i = new Intent(getApplicationContext(), KullaniciGirisi.class);
+                                i.setFlags(i.FLAG_ACTIVITY_CLEAR_TOP | i.FLAG_ACTIVITY_CLEAR_TASK | i.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(i);
+                                //startActivity(new Intent(Activity_StartMenu.this, KullaniciGirisi.class));
+                            }*/
+                        }
+                        //istenilen event gerçekleştikten sonra drawer'ı kapat ->
+                        return false;
+                    }
+                })
+                .build();
+
+
     }
 }
